313c6cfeadcbc04426afa77dc1b873f83a44b958
Jonny Vorn Soth <janysoth@gmail.com>
2024-09-22 04:20:41 -0500

    Add a button to toggle from dark to light mode.

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Button,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
-  useColorScheme,
 } from 'react-native';
 
 const AppPro = (): JSX.Element => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, isDarkMode ? styles.darkBackground : styles.lightBackground]}>
+      <TouchableOpacity style={styles.button} onPress={toggleTheme}>
+        <Text style={styles.buttonText}>Toggle Theme</Text>
+      </TouchableOpacity>
       <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
         Hello World!
       </Text>
-    </View >
+    </View>
   );
 };
 
@@ -23,12 +31,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
   },
-
+  button: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: 'red', // Light blue background
+    padding: 10,
+    borderRadius: 5, // Optional: rounded corners
+  },
+  buttonText: {
+    color: '#FFFFFF', // White text
+  },
+  darkBackground: {
+    backgroundColor: '#000000', // Dark background for dark mode
+  },
+  lightBackground: {
+    backgroundColor: '#FFFFFF', // Light background for light mode
+  },
   whiteText: {
     color: '#FFFFFF',
   },
-
   darkText: {
     color: '#000000',
   },
